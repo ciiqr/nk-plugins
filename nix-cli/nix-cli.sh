df5c2536a109a15f9aeea075a0cddb75d1739ab9
William Villeneuve <ciiqr@users.noreply.github.com>
2023-04-30 19:50:10 -0400

    nix-cli: now runs garbage collection

--- nix-cli/nix-cli.sh
+++ nix-cli/nix-cli.sh
@@ -24,6 +24,31 @@ nix_cli::_provision_nix() {
     fi
 }
 
+nix_cli::_provision_gc() {
+    # `nix-store` on path
+    if type nix-store >/dev/null 2>&1; then
+        declare dead_store_output
+        dead_store_output="$(nix-store --gc --print-dead --quiet)" || return "$(nk::error "$?" 'failed to list dead paths')"
+        declare -a dead_store_paths
+        while read -r file; do
+            dead_store_paths+=("$file")
+        done <<< "$dead_store_output"
+
+        # at least one dead path
+        if [[ "${#dead_store_paths[@]}" -gt 0 ]]; then
+            declare disk_usage
+            disk_usage="$(du -mc "${dead_store_paths[@]}" | tail -n1 | awk '{print $1}')" \
+                || return "$(nk::error "$?" 'failed to get disk usage of dead paths')"
+
+            # disk usage > 1G
+            if (( disk_usage > 1000 )); then
+                nix-store --gc || return "$?"
+                changed='true'
+            fi
+        fi
+    fi
+}
+
 declare action='install'
 declare status='success'
 declare changed='false'
@@ -36,4 +61,17 @@ nk::log_result \
     "$status" \
     "$changed" \
     "${action} package nix" \
+    "$output"
+
+declare status='success'
+declare changed='false'
+declare output=''
+if ! nk::run_for_output output nix_cli::_provision_gc; then
+    status='failed'
+fi
+
+nk::log_result \
+    "$status" \
+    "$changed" \
+    'nix store clean' \
     "$output"
