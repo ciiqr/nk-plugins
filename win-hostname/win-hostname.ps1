5c969eb9649cbe57188824b74906dc5c3631ff8b
William Villeneuve <ciiqr@users.noreply.github.com>
2023-10-15 20:23:25 -0400

    added win-hostname plugin

---
+++ win-hostname/win-hostname.ps1
@@ -0,0 +1,55 @@
+Param (
+    [Parameter(Mandatory)]
+    [ValidateSet("provision")]
+    [string]$command,
+
+    [Parameter(Mandatory)]
+    [string]$info_json,
+
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [string]$stdin
+)
+
+function hostname_provision_computer_name() {
+    Param (
+        [Parameter(Mandatory)]
+        [string]$hostname
+    )
+
+    $result = @{
+        status = "success"
+        changed = $false
+        description = "hostname ${hostname}"
+        output = ""
+    }
+
+    if ($env:ComputerName -ne $hostname) {
+        $result.changed = $true
+        $result.output = (Rename-Computer -NewName $hostname -Force) -join "`n"
+        if (!$?) {
+            $result.status = "failed"
+            return $result
+        }
+    }
+
+    return $result
+}
+
+function hostname_provision() {
+    $states = ConvertFrom-Json $stdin
+
+    # provision hostname
+    foreach ($state in $states) {
+        hostname_provision_computer_name $state.state | ConvertTo-Json -Compress
+    }
+}
+
+switch ($command) {
+    "provision" {
+        hostname_provision
+    }
+    default {
+        Write-Output "hostname: unrecognized subcommand: ${command}"
+        exit 1
+    }
+}
